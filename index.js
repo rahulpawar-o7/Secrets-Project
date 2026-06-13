bfbd8b191e013967e62df0e72f6e747de26be820
Ranul <pawar01214@gmail.com>
2026-06-13 17:17:21 +0530

    Add /register in js

--- index.js
+++ index.js
@@ -110,7 +110,38 @@ app.post(
   })
 );
 
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
 
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (checkResult.rows.length > 0) {
+      req.redirect("/login");
+    } else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+        } else {
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+            [email, hash]
+          );
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log("success");
+            res.redirect("/secrets");
+          });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 
 passport.serializeUser((user, cb) => {
