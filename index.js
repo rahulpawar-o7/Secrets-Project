916a0359a45570336c937ee6114aadb3dffcfb06
Ranul <pawar01214@gmail.com>
2026-06-25 20:18:56 +0530

    index.js 1.7

--- index.js
+++ index.js
@@ -160,6 +160,41 @@ app.post("/submit", async (req, res) => {
 });
 
 
+ 
+passport.use(
+  "local",
+  new Strategy(async function verify(username, password, cb) {
+    try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
+        username,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            console.error("Error comparing passwords:", err);
+            return cb(err);
+          } else {
+            if (valid) {
+              return cb(null, user);
+            } else {
+              return cb(null, false);
+            }
+          }
+        });
+      } else {
+        return cb("User not found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+);
+
+
+
+
 passport.serializeUser((user, cb) => {
   cb(null, user);
 });
